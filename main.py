0ff7740bfe793753fcfa27e03518e719fbf44e3c
Isabel Cachola <isabelcachola@utexas.edu>
2021-05-04 17:45:49 -0400

    crf model for srl done

--- main.py
+++ main.py
@@ -4,41 +4,95 @@ Main script for training and testing models
 import argparse
 import logging
 import time
+from ipdb.__main__ import set_trace
+import tqdm
+import json
+import os
+import pickle
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
+from nltk.tokenize import TweetTokenizer
 
-def read_data(dataset, datadir):
+from data import RemanData, TweetData
+from models import LogisticRegression, CRF, MLP
+from utils import build_bow_reman, build_bow_tweet
+
+def read_data(dataset, datadir, split='train'):
     if dataset == 'reman':
-        NotImplementedError()
+        data = RemanData(datadir, split)
+        data.read_data()
+
+        # Either load bow or build bow
+        dict_path = os.path.join(datadir, 'reman-vocab.json')
+        if os.path.exists(dict_path) or split !='train':
+            dicts = json.load(open(dict_path))
+        else:
+            nlp = English()
+            tokenizer = Tokenizer(nlp.vocab)
+            dicts = build_bow_reman(data, tokenizer)
+            with open(dict_path, 'w') as fout:
+                json.dump(dicts, fout, indent=4)
 
     elif dataset == 'tweet':
-        NotImplementedError()
+        data = TweetData(datadir, split)
+        data.read_data()
+
+        dict_path = os.path.join(datadir, 'tweet-vocab.json')
+        if os.path.exists(dict_path)  or split !='train':
+            dicts = json.load(open(dict_path))
+        else:
+            tokenizer = TweetTokenizer().tokenize
+            dicts = build_bow_tweet(data, tokenizer)
+            with open(dict_path, 'w') as fout:
+                json.dump(dicts, fout, indent=4)
+
     else:
         raise ValueError(f'Uknown dataset type {dataset}')
 
+    logging.info(f'Vocab size: {len(dicts["ind2w"])}')
+
+    return data, dicts
+
 def init_model(model_type):
     if model_type =='logreg':
-        NotImplementedError()
+        model = LogisticRegression()
     elif model_type == 'crf':
-        NotImplementedError()
+        model = CRF()
     elif model_type == 'mlp':
-        NotImplementedError()
+        model = MLP()
     else:
         raise ValueError(f'Uknown model type {model_type}')
 
-def train(model, data):
-    NotImplementedError()
+    return model
+
+def train(model, train_data, dev_data, outdir):
+    X_train, y_train = model.featurize_data(train_data)
+    model.train(X_train, y_train)
+
+    X_dev, y_dev = model.featurize_data(dev_data)
+    model.test(X_dev, y_dev)
+
+    return model
 
 def test(model, data):
-    NotImplementedError()
+    X_test, y_test = model.featurize_data(data)
+    model.test(X_test, y_test)
 
 def main(args):
-    data = read_data(args.dataset, args.datadir)
-
     model = init_model(args.model_type)
+    model_path = os.path.join(args.outdir, f'{args.dataset}-{args.model_type}.joblib')
 
     if args.mode == 'train':
-        model = train(model, data, args.outdir)
+        train_data, dicts = read_data(args.dataset, args.datadir, split='train')
+        dev_data, _ = read_data(args.dataset, args.datadir, split='dev')
+        model = train(model, train_data, dev_data, args.outdir)
+        model.save(model_path)
+
     elif args.mode == 'test':
-        test(model, data)
+        model.load(model_path)
+        test_data, dicts = read_data(args.dataset, args.datadir, split='test')
+        test(model, test_data)
+        
     else:
         ValueError(f'Unkown mode {args.mode}')
 
---
+++ models.py
@@ -0,0 +1,100 @@
+import os
+import sklearn_crfsuite
+from sklearn_crfsuite import metrics
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import RandomizedSearchCV
+import scipy
+import nltk
+import joblib
+
+class LogisticRegression:
+    def __init__(self):
+        pass
+
+class CRF:
+    def __init__(self):
+        self.crf = sklearn_crfsuite.CRF(
+            algorithm='lbfgs',
+            c1=0.1,
+            c2=0.1,
+            max_iterations=100,
+            all_possible_transitions=True
+        )
+        self.labels = ['experiencer', 'emotion', 'target']
+
+    def featurize_data(self, data):
+        X = []
+        y =[]
+        
+        for i, doc in enumerate(data):
+            srl_labels = doc.get_srl_labels()
+            X.append(self.sent2features(srl_labels))
+            y.append(self.sent2labels(srl_labels))
+
+        return X, y
+
+    def sent2labels(self, sent):
+        return [word[1] for word in sent]
+
+    def sent2features(self, sent):
+        return [self.word2features(sent, i) for i in range(len(sent))]
+
+    def word2features(self, doc, i):
+        word = doc[i][0]
+
+        # Common features for all words. You may add more features here based on your custom use case
+        features = [
+                'bias',
+                'word.lower=' + word.lower(),
+                'word[-3:]=' + word[-3:],
+                'word[-2:]=' + word[-2:],
+                'word.isupper=%s' % word.isupper(),
+                'word.isdigit=%s' % word.isdigit()
+            ]
+
+        # Features for words that are not at the beginning of a document
+        if i > 0:
+                word1 = doc[i-1][0]
+                features.extend([
+                    '-1:word.lower=' + word1.lower(),
+                    '-1:word.istitle=%s' % word1.istitle(),
+                    '-1:word.isupper=%s' % word1.isupper(),
+                    '-1:word.isdigit=%s' % word1.isdigit()
+                ])
+        else:
+            # Indicate that it is the 'beginning of a document'
+            features.append('BOS')
+
+        # Features for words that are not at the end of a document
+        if i < len(doc)-1:
+                word1 = doc[i+1][0]
+                features.extend([
+                    '+1:word.lower=' + word1.lower(),
+                    '+1:word.isupper=%s' % word1.isupper(),
+                    '+1:word.isdigit=%s' % word1.isdigit()
+                ])
+        else:
+            # Indicate that it is the 'end of a document'
+            features.append('EOS')
+
+        return features
+
+    def train(self, X, y):
+        self.crf.fit(X, y)
+
+    def test(self, X, y):
+        y_pred = self.crf.predict(X)
+        labels = list(self.crf.classes_)
+        labels.remove('O')
+        print(metrics.flat_classification_report(y, y_pred,
+                            labels=labels))
+
+    def save(self, outpath):
+        joblib.dump(self.crf, outpath)
+    
+    def load(self, outpath):
+        self.crf = joblib.load(outpath)
+    
+class MLP:
+    def __init__(self):
+        pass
